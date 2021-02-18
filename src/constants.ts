969f24981dd0d9a1d07cce8005a1f592b6e97ab8
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-02-18 19:51:07 +0100

    🎉 First commit

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+import OSS, {Options} from 'ali-oss';
+import {getInput} from '@actions/core';
+import {join} from 'path';
+
+export const credentials: Options = {
+  bucket: getInput('bucket'),
+  region: getInput('region'),
+  accessKeyId: getInput('accessKeyId'),
+  accessKeySecret: getInput('accessKeySecret')
+};
+
+export const homeDir: string = join(process.cwd(), getInput('source'), '/');
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import OSS, {PutObjectResult} from 'ali-oss';
+import {info, warning} from '@actions/core';
+import {create as createGlobber, Globber} from '@actions/glob';
+import {credentials, homeDir} from './constants';
+import {join} from 'path';
+
+async function upload(): Promise<void> {
+  try {
+    const client: OSS = new OSS(credentials);
+    const uploadDir: Globber = await createGlobber(join(homeDir, '**/*.*'));
+    const localFiles: string[] = await uploadDir.glob();
+    const size: number = localFiles.length;
+    let index: number = 0;
+    let percent: number = 0;
+    info(`${size} files to upload`);
+    for await (const file of uploadDir.globGenerator()) {
+      const objectName: string = file.replace(homeDir, '');
+      const response: PutObjectResult = await client.put(objectName, file);
+      index++;
+      percent = (index / size) * 100;
+      info(`\u001b[38;5;6m>> [${index}/${size}, ${percent.toFixed(2)}%] uploaded: ${response.name}`);
+    }
+  } catch (error) {
+    warning(error.message);
+  }
+}
+
+upload();
