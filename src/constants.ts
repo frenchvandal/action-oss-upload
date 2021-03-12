0a03db6d612970cd6bddfc6b6751f4467548ea40
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-12 21:26:52 +0100

    toto

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,14 @@
-import {Options} from 'ali-oss';
+import {Config} from '@alicloud/oss-client';
 import {getInput} from '@actions/core';
 import {join, sep} from 'path';
 
-export const credentials: Options = {
+export const credentials: Config = new Config({
   bucket: getInput('bucket'),
   region: getInput('region'),
   accessKeyId: getInput('accessKeyId'),
-  accessKeySecret: getInput('accessKeySecret')
-};
+  accessKeySecret: getInput('accessKeySecret'),
+});
 
 export const homeDir: string = join(process.cwd(), getInput('source'), sep);
 
-export const pattern: string = getInput('pattern') || '**'.concat(sep, '*.*');
+export const pattern: string = getInput('pattern') ?? '**'.concat(sep, '*.*');
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,36 @@
-import OSS, {PutObjectResult} from 'ali-oss';
+import Client, {PutObjectRequest, PutObjectResponse} from '@alicloud/oss-client';
 import {info, setFailed} from '@actions/core';
 import {create as createGlobber, Globber} from '@actions/glob';
 import {credentials, homeDir, pattern} from './constants';
 
-const IS_WINDOWS: boolean = process.platform === 'win32';
+const isWindows: boolean = process.platform === 'win32';
 
 async function upload(): Promise<void> {
   try {
-    info(`Runner is ${process.env.RUNNER_OS}`);
-    const client: OSS = new OSS(credentials);
+    const client: Client = new Client(credentials);
     const uploadDir: Globber = await createGlobber(homeDir.concat(pattern));
     let localFiles: string[] = await uploadDir.glob();
-    if (IS_WINDOWS) {
-      localFiles = localFiles.map(item => item.replace(/\\/g, '/'));
+    if (isWindows) {
+      localFiles = localFiles.map((item) => item.replace(/\\/g, '/'));
     }
     const size: number = localFiles.length;
     let index: number = 0;
     let percent: number = 0;
-    info(`⬆️ ${size} files to upload`);
+    info(`${size} files to upload`);
     for await (const file of localFiles) {
       const objectName: string = file.replace(homeDir, '');
-      const response: PutObjectResult = await client.put(objectName, file);
+      const request: PutObjectRequest  = new PutObjectRequest({objectName, file});
+      let runtime: any;
+      const response: PutObjectResponse = await client.putObject(request, runtime);
       index++;
       percent = (index / size) * 100;
-      info(`\u001b[38;5;6m>> [${index}/${size}, ${percent.toFixed(2)}%] uploaded: ${response.name}`);
+      info(
+        `\u001b[38;5;6m>> [${index}/${size}, ${percent.toFixed(
+          2,
+        )}%] uploaded: ${response.name}`,
+      );
     }
-    info(`✅ ${index} files uploaded`);
+    info(`${index} files uploaded`);
   } catch (e) {
     setFailed(e.message);
   }
