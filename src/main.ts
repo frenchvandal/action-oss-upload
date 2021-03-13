1b0085b271630359847c8c29025abda0050fe086
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-13 17:25:37 +0100

    ;ll

--- src/main.ts
+++ src/main.ts
@@ -35,7 +35,7 @@ async function upload(): Promise<void> {
     for await (const file of localFiles) {
       let objectName: string = file.replace(homeDir, '');
 
-      if (isWindows) objectName.replace(/\\/g, posix.sep);
+      if (isWindows) objectName.replace(/\\/g, '/');
 
       info(objectName);
       const response: PutObjectResult = await client.put(objectName, file);
