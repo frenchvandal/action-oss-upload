b4a9ae929d898afa10a25b01a6bc30445ec4752d
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-13 09:12:41 +0100

    tptpp

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import {error, info, setFailed} from '@actions/core';
+import {info, setFailed} from '@actions/core';
 import {create as createGlobber, Globber} from '@actions/glob';
 import {getInput, homeDir, pattern} from './constants';
-import {ObjectPutReturnType} from 'ali-oss/lib/types/object';
-import {IOptions} from 'ali-oss/lib/types/params';
-import Client from 'ali-oss';
+import OSS, {Options, PutObjectResult} from 'ali-oss';
+import {basename} from 'path';
 
-const isWindows: boolean = process.platform === 'win32';
-
-const credentials: IOptions = {
+const credentials: Options = {
   accessKeyId: getInput('accessKeyId'),
   accessKeySecret: getInput('accessKeySecret'),
   bucket: getInput('bucket'),
@@ -16,19 +13,16 @@ const credentials: IOptions = {
 
 async function upload(): Promise<void> {
   try {
-    const client: Client = new Client(credentials);
+    const client: OSS = new OSS(credentials);
     const uploadDir: Globber = await createGlobber(homeDir.concat(pattern));
     let localFiles: string[] = await uploadDir.glob();
-    if (isWindows) {
-      localFiles = localFiles.map((item) => item.replace(/\\/g, '/'));
-    }
     const size: number = localFiles.length;
     let index: number = 0;
     let percent: number = 0;
     info(`${size} files to upload`);
     for await (const file of localFiles) {
-      const objectName: string = file.replace(homeDir, '');
-      const response: ObjectPutReturnType = await client.put(objectName, file);
+      const objectName: string = basename(file);
+      const response: PutObjectResult = await client.put(objectName, file);
       index++;
       percent = (index / size) * 100;
       info(
@@ -39,7 +33,6 @@ async function upload(): Promise<void> {
     }
     info(`${index} files uploaded`);
   } catch (e) {
-    error(e.message);
     setFailed(e.message);
   }
 }
