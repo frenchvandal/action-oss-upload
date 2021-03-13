21bb852624b3c75451420c0ab378b38a8d0acde9
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-13 16:23:52 +0100

    kkk

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import {info, setFailed} from '@actions/core';
+import {getInput, info, setFailed} from '@actions/core';
 import {create, Globber} from '@actions/glob';
-import {getInput, homeDir, pattern, posix} from './constants';
 import OSS, {Options, PutObjectResult} from 'ali-oss';
+import {join, posix, sep} from 'path';
+
+const isWindows = process.platform === 'win32';
+
+const homeDir: string = join(process.cwd(), getInput('source'), sep);
+
+const pattern: string = '**'.concat(sep, '*.*');
 
 const credentials: Options = {
   accessKeyId: getInput('accessKeyId'),
@@ -10,27 +16,36 @@ const credentials: Options = {
   region: getInput('region'),
 };
 
+const client: OSS = new OSS(credentials);
+
 async function upload(): Promise<void> {
   try {
-    //const client: OSS = new OSS(credentials);
     info(`\u001b[38;5;6m>>homeDir: ${homeDir}`);
     info(`\u001b[38;5;6m>>pattern: ${pattern}`);
-    const uploadDir: Globber = await create(homeDir.concat(pattern));
-    const size: number = (await uploadDir.glob()).length;
     let index: number = 0;
     let percent: number = 0;
+    const uploadDir: Globber = await create(homeDir.concat(pattern));
+    const size: number = (await uploadDir.glob()).length;
+    const localFiles: AsyncGenerator<
+      string,
+      void,
+      unknown
+    > = uploadDir.globGenerator();
     info(`${size} files to upload`);
-    for await (const file of uploadDir.globGenerator()) {
-      const objectName: string = posix.basename(file);
+    for await (const file of localFiles) {
+      let objectName: string = file.replace(homeDir, '');
+
+      if (isWindows) objectName = file.replace(sep, posix.sep);
+
       info(objectName);
-      //const response: PutObjectResult = await client.put(objectName, file);
+      const response: PutObjectResult = await client.put(objectName, file);
 
       index++;
       percent = (index / size) * 100;
       info(
         `\u001b[38;5;6m>> [${index}/${size}, ${percent.toFixed(
           2,
-        )}%] uploaded: ${objectName}`,
+        )}%] uploaded: ${response.name}`,
       );
     }
     info(`${index} files uploaded`);
