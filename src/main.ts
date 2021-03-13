7a217f670ebef845a598cee42355fadc2f77a607
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-03-13 20:20:28 +0100

    prout

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,65 @@
-import {getInput, info} from '@actions/core';
-import {create, Globber} from '@actions/glob';
-import OSS, {Options, PutObjectResult} from 'ali-oss';
-import {join, posix, sep} from 'path';
+import {getInput, info} from '@actions/core'
+import {create, Globber} from '@actions/glob'
+import OSS, {Options, PutObjectResult} from 'ali-oss'
+import {join, posix, sep} from 'path'
 
-const isWindows: boolean = process.platform === 'win32';
+const isWindows: boolean = process.platform === 'win32'
 
-const homeDir: string = join(process.cwd(), getInput('source'), sep);
+const homeDir: string = join(process.cwd(), getInput('source'), sep)
 
-const pattern: string = `**${sep}*.*`;
+const pattern: string = `**${sep}*.*`
 
 const credentials: Options = {
   accessKeyId: getInput('accessKeyId'),
   accessKeySecret: getInput('accessKeySecret'),
   bucket: getInput('bucket'),
   region: getInput('region'),
-};
+}
 
-const client: OSS = new OSS(credentials);
+const client: OSS = new OSS(credentials)
 
 async function upload(): Promise<void> {
   try {
-    let index: number = 0;
-    let percent: number = 0;
+    let index: number = 0
+    let percent: number = 0
 
-    const uploadDir: Globber = await create(`${homeDir}${pattern}`);
-    const size: number = (await uploadDir.glob()).length;
+    const uploadDir: Globber = await create(`${homeDir}${pattern}`)
+    const size: number = (await uploadDir.glob()).length
     const localFiles: AsyncGenerator<
       string,
       void,
       unknown
-    > = uploadDir.globGenerator();
+    > = uploadDir.globGenerator()
 
-    info(`${size} files to upload`);
+    info(`${size} files to upload`)
 
     for await (const file of localFiles) {
-      let objectName: string = file.replace(homeDir, '');
+      let objectName: string = file.replace(homeDir, '')
 
       if (isWindows) {
         objectName = objectName.replace(
           new RegExp(`\\${sep}`, 'g'),
           `${posix.sep}`,
-        );
+        )
       }
 
-      const response: PutObjectResult = await client.put(objectName, file);
+      const response: PutObjectResult = await client.put(objectName, file)
 
-      index++;
-      percent = (index / size) * 100;
+      index++
+      percent = (index / size) * 100
 
       info(
         `\u001b[38;5;6m>> [${index}/${size}, ${percent.toFixed(
           2,
         )}%] uploaded: ${response.name}`,
-      );
+      )
     }
 
-    info(`${index} files uploaded`);
+    info(`${index} files uploaded`)
   } catch (error) {
-    const {setFailed} = await import('@actions/core');
-    setFailed(error.message);
+    const {setFailed} = await import('@actions/core')
+    setFailed(error.message)
   }
 }
 
-upload();
+upload()
