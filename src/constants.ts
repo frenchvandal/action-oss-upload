77ca612c55a320fb9cef2fd684e272ac0ccdbe57
Philippe <42499534+frenchvandal@users.noreply.github.com>
2021-03-13 10:06:11 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,6 @@ import {join, sep} from 'path';
 
 export const homeDir: string = join(process.cwd(), getInput('source'), sep);
 
-export const pattern: string = getInput('pattern') ?? '**'.concat(sep, '*.*');
+export const pattern: string = '**'.concat(sep, '*.*');
 
 export {getInput};
