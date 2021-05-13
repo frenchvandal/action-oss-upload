170d245da87eda7cfbbd801990443a13af019c2f
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-05-13 20:01:41 +0200

    LOL

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        'comma-dangle': ['error', 'always-multiline'],
+        curly: ['error', 'multi-or-nest'],
+      },
+    ],
+  },
+};
